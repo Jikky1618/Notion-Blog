73b8a35627d4a1fc78b395ceaabab82d98f0fa1e
Jikky1618 <Jikky0110@gmail.com>
2023-01-06 08:04:18 +0900

    init

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home({ posts }) {
           </div>
           <h1>Next.js + Notion API ブログ</h1>
           <p>
-           Notionと連携しているブログです。Notionに書き込めばそのままブログとして投稿できます。
+            Notionと連携しているブログです。Notionに書き込めばそのままブログとして投稿できます。
           </p>
         </header>
 
@@ -87,5 +87,14 @@ export default function Home({ posts }) {
   );
 }
 
-//SSGを追加
+//ISRを追加
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
 
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  };
+};
